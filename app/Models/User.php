b062add05b875e255f866dbaf934077d53770af6
devdrian <matiose12@gmail.com>
2025-01-07 01:09:56 -0500

    cambios

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,6 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
-
     /**
      * The attributes that are mass assignable.
      *
